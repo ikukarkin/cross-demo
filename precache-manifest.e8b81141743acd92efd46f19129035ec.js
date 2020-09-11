cd7086224263b4da3bf4490e24820cd1e22b425a
--global <--global>
2020-09-11 15:50:18 +0500

    Updates

---
+++ precache-manifest.e8b81141743acd92efd46f19129035ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0265a97d0220649d1bf79055f4060ea3",
+    "url": "/cross-demo/index.html"
+  },
+  {
+    "revision": "9bfb27197ac585f836e6",
+    "url": "/cross-demo/static/css/main.6f2cd681.chunk.css"
+  },
+  {
+    "revision": "f803dd14589a41dd6e44",
+    "url": "/cross-demo/static/js/2.2a0e8a70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cross-demo/static/js/2.2a0e8a70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bfb27197ac585f836e6",
+    "url": "/cross-demo/static/js/main.6163987c.chunk.js"
+  },
+  {
+    "revision": "b7ab676a0390a818b873",
+    "url": "/cross-demo/static/js/runtime-main.61f15f1c.js"
+  }
+]);
